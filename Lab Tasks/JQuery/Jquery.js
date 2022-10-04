cfe608e64cb4f7b04bd5f5ff086e9cb63b34d43e
BMani90 <sp20-bcs-138@cuilahore.edu.pk>
2022-10-05 01:00:52 +0500

    JQuery

---
+++ Lab Tasks/JQuery/Jquery.js
@@ -0,0 +1,7 @@
+function clrChange() {
+  var value = $("#email").val();
+  if (value === "") {
+    $("#email").addClass("red");
+    $("#email").attr("placeholder", "Please enter your Email");
+  } else $("#email").removeClass("red");
+}
