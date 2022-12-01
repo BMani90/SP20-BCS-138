f75ade36daafd95d2741f06fac089e63cb1a49ae
BMani90 <sp20-bcs-138@cuilahore.edu.pk>
2022-12-02 03:15:12 +0500

    body color change

--- Web-Lab-Mid (SP20-BCS-138_A)/Event.js
+++ Web-Lab-Mid (SP20-BCS-138_A)/Event.js
@@ -1,18 +1,17 @@
-// function setColor(btn, color) {
-//   var count = 1;
-//   var property = document.getElementById(btn);
-//   if (count == 0) {
-//     property.style.backgroundColor = "#FFFFFF";
-//     count = 1;
-//   } else {
-//     property.style.backgroundColor = "Orange";
-//     count = 0;
-//   }
-// }
+//CHANGING BACKGROUND COLOR OF BUTTON
 
+// window.onload = function () {
+//   let btn = document.getElementById("btn2");
+//   btn.onclick = function () {
+//     btn.style.backgroundColor = "Orange";
+//   };
+// };
+
+///CHANGING BACKGROUND COLOR OF HTML BODY
 window.onload = function () {
   let btn = document.getElementById("btn2");
   btn.onclick = function () {
-    btn.style.backgroundColor = "Orange";
+    let clr = document.body;
+    clr.style.backgroundColor = "Orange";
   };
 };
